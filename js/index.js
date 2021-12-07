ec165b2fd3c860878e6932044c6e8d79681ad0fa
Umidjon <umidzhonofficialdev@gmail.com>
2021-12-07 23:57:44 +0600

    Korzinka in Vanilla js

--- js/index.js
+++ js/index.js
@@ -4,26 +4,25 @@ const korzinka = document.querySelector("#korzinka");
 const buyBtns = document.querySelectorAll("#buy__btn");
 const productCards = document.querySelectorAll(".product__card")
 const korzinkaContent = document.querySelector(".korzinka__cards")
-
 openModalBtn.addEventListener("click", () => {
     korzinka.classList.add("visible");
+    document.querySelector(".korzina__content").classList.add("hidden")
 })
 closeModalBtn.addEventListener("click", () => {
     korzinka.classList.remove("visible");
+    document.querySelector(".korzina__content").classList.remove("hidden")
 })
 buyBtns.forEach((btn, index) => {
     btn.addEventListener("click", () => {
-
         const newCard = document.createElement("div");
         newCard.innerHTML = `
             <img src="${btn.previousElementSibling.children[0].getAttribute("src")}" />
         `
         newCard.classList.add("korzinka__card");
-
-        korzinkaContent.append(newCard)
+        korzinkaContent.appendChild(newCard)
         productCards[index].remove();
     })
 })
 
-const test = document.querySelector(".test");
+
 
